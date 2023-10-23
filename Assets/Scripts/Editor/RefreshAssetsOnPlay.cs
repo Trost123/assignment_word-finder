9814fda37436417243d13b164b2144badd279ba0
Trost <ekolodya@gmail.com>
2023-10-23 15:59:05 +0300

    QoL plugins: Refresh assets on play, fast script reload.

---
+++ Assets/Scripts/Editor/RefreshAssetsOnPlay.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+
+namespace Editor
+{
+    [InitializeOnLoad]
+    public static class RefreshAssetsOnPlay
+    {
+        /// <summary>
+        /// Subscribe to the playmode state change event
+        /// </summary>
+        static RefreshAssetsOnPlay()
+        {
+            EditorApplication.playModeStateChanged += PlayRefresh;
+        }
+        
+        /// <summary>
+        /// Refresh assets when playmode is entered
+        /// </summary>
+        private static void PlayRefresh(PlayModeStateChange state) {
+            if(state == PlayModeStateChange.ExitingEditMode) {
+                AssetDatabase.Refresh();
+            }
+        }
+    }
+}

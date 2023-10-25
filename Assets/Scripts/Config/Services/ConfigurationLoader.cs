a69951e8e0d07fc22460d802105bd5717a688e95
Trost <ekolodya@gmail.com>
2023-10-25 17:21:41 +0300

    Load JSON from filesystem instead of using addressables.

--- Assets/Scripts/Config/Services/ConfigurationLoader.cs
+++ Assets/Scripts/Config/Services/ConfigurationLoader.cs
@@ -1,9 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 using JetBrains.Annotations;
 using Newtonsoft.Json;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
-using UnityEngine.ResourceManagement.AsyncOperations;
 
 namespace Config.Services
 {
@@ -12,16 +12,18 @@ namespace Config.Services
     {
         public async Task<T> LoadConfigAsync<T>(string path)
         {
-            var handle = Addressables.LoadAssetAsync<TextAsset>(path);
-            await handle.Task;
-
-            if (handle.Status == AsyncOperationStatus.Succeeded)
+            try
             {
-                return JsonConvert.DeserializeObject<T>(handle.Result.text);
+                // Read the file asynchronously
+                using var streamReader = new StreamReader(path);
+                string jsonText = await streamReader.ReadToEndAsync();
+                return JsonConvert.DeserializeObject<T>(jsonText);
             }
-
-            Debug.LogError($"Failed to load config from path: {path}, Status: {handle.Status}");
-            return default;  // Returns null for reference types, zero for numeric types, etc.
+            catch (Exception ex)
+            {
+                Debug.LogError($"Failed to load config from path: {path}, Exception: {ex.Message}");
+                return default;  // Returns null for reference types, zero for numeric types, etc.
+            }
         }
     }
 }
